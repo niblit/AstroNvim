6759b90a49dc4d893b409a1f910b57e4eb621e4e
niblit <85725939+niblit@users.noreply.github.com>
2023-05-22 11:25:27 -0600

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,13 @@
+return {
+  colorscheme = "catppuccin",
+
+  plugins = {
+    {
+      "catppuccin/nvim",
+      name = "catppuccin",
+      config = function()
+        require("catppuccin").setup {}
+      end,
+    },
+  },
+}
