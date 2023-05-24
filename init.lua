876e6d0f01e4b277fcc125dd56a3fb40bab2dd54
niblit <github.com/niblit>
2023-05-23 21:32:41 -0600

    Added astrocommunity plugins

--- init.lua
+++ init.lua
@@ -3,7 +3,12 @@ return {
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.colorscheme.catppuccin" },
     { import = "astrocommunity.colorscheme.rose-pine" },
-    { import = "astrocommunity.pack.rust" },
-  }
-}
+    { import = "astrocommunity.colorscheme.dracula" },
 
+    { import = "astrocommunity.pack.bash" },
+    { import = "astrocommunity.pack.haskell" },
+    { import = "astrocommunity.pack.markdown" },
+    { import = "astrocommunity.pack.python" },
+    { import = "astrocommunity.pack.rust" },
+  },
+}
