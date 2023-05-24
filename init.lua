6dd89ae56e9b9fed5e320fb2e703a78facf90f02
niblit <github.com/niblit>
2023-05-23 21:43:47 -0600

    Remove plugins

--- init.lua
+++ init.lua
@@ -5,10 +5,6 @@ return {
     { import = "astrocommunity.colorscheme.rose-pine" },
     { import = "astrocommunity.colorscheme.dracula" },
 
-    { import = "astrocommunity.pack.bash" },
-    { import = "astrocommunity.pack.haskell" },
-    { import = "astrocommunity.pack.markdown" },
-    { import = "astrocommunity.pack.python" },
     { import = "astrocommunity.pack.rust" },
   },
 }
